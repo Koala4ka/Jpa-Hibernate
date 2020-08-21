a5dfce9ed7fc0395f9c827cfee0ece45710cab29
koala4ka <gvani@ukr.net>
2020-08-21 23:55:24 +0300

    created config and added propetrties for hibernate

---
+++ src/main/java/com/anna/Application.java
@@ -0,0 +1,5 @@
+package com.anna;
+
+public class Application {
+
+}
---
+++ src/main/java/com/anna/entity/Book.java
@@ -0,0 +1,10 @@
+package com.anna.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+public class Book {
+}
